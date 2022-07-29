e2d7706cb006ef6c77c4bebbee6cde6da9d71368
Sush Krishnamoorthy <sushkrsh@gmail.com>
2022-07-29 04:45:16 +0000

    collections practice problems

---
+++ rb101/4_lesson/practice.rb
@@ -0,0 +1,10 @@
+#Ex1. [] no element meets selection criterion
+#Ex2. 2, as a filter before counting
+#Ex3. [1,2,3], puts returns niil which is false leading to no rejection
+#Ex4. {'a':'ant', 'b':bear, 'c':'cat'}
+#Ex5. returns [a,'ant'] and modified hash = {b:'bear'}
+#Ex6. 11 pop removes & returns the last element, size gives length.
+#Ex7. true, 1 & 3 are odd. 
+#Ex8. [1,2] and doesn't modify the original array
+#Ex9. [nil, 'bear'] false condition returns nil for the ma
+#Ex10. [1,nil,nil]
