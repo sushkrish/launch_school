dbe288b05d2434b941cf5de65a697d64ac2593aa
sushkrish <sushkrsh@gmail.com>
2022-08-04 17:07:31 -0400

    Delete rb101/easy7 directory

--- rb101/easy7/1.rb
+++
@@ -1,5 +0,0 @@
-def interleave(array1, array2)
-  array1.zip(array2).flatten
-end
-
-puts interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
--- rb101/easy7/8.rb
+++
@@ -1,5 +0,0 @@
-def multiply_list(arr1, arr2)
-  arr1.zip(arr2).map {|(a,b)| a*b }
-end
-
-puts multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
