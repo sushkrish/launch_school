53cb46c007e7280589e46da42866940d8e809287
sushkrish <sushkrsh@gmail.com>
2022-08-04 17:07:21 -0400

    Delete rb101/easy6 directory

--- rb101/easy6/9.rb
+++
@@ -1,9 +0,0 @@
-def include?(arr,n)
-  arr.any? {|i| i == n}
-end
-
-puts include?([1,2,3,4,5], 3) == true
-puts include?([1,2,3,4,5], 6) == false
-puts include?([], 3) == false
-puts include?([nil], nil) == true
-puts include?([], nil) == false
