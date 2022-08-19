811abb8c23f59035ec9bcdd4ebbfc98ba6b63595
Sush Krishnamoorthy <sushkrsh@gmail.com>
2022-08-19 16:55:08 +0000

    more practice problems

---
+++ rb120/3_lesson/1_medium/written.rb
@@ -0,0 +1,13 @@
+#1. Ben. there is a reader method through attr_reader
+
+#2. There's no writer for quantity. Need writer or @quantity
+
+#3. Provides additional access for product_name as well. (Alters the public interface) 
+
+#4. see code in dir
+
+#5. see code in dir
+
+#6. self.template will call the functions definted through the attr_accessor. 
+
+#7. change to status
---
+++ rb120/3_lesson/3.rb
@@ -0,0 +1,13 @@
+#1. "Hello", error, error, "Goodbye", error
+
+#2. Make hi a class function def self.hi
+
+#3. AngryCat.new(5, "fluffly") && AngryCat.new(6, "buffy")
+
+#4. def to_s; "I'm a #{type} cat"; end
+
+#5. can't call tv.manufacturer or Television.model
+
+#6. @age
+
+#7. return keyword in information method, accessors
