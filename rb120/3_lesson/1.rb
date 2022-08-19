2a420f717ed7da7a4b3e67a9e030ab8ac3f8e7e9
Sush Krishnamoorthy <sushkrsh@gmail.com>
2022-08-19 16:14:44 +0000

    lesson 3 practice problems

---
+++ rb120/3_lesson/1.rb
@@ -0,0 +1,19 @@
+# 1,2,3,4 are all objects. using Object#class method. 
+
+# include Speed module in the Car, TRuck classes. 
+
+# through self.class which returns the class name of the calling object.
+
+# AngryCat.new
+
+# Pizza, from the @. Can create an object and call .instance_variables on that instance to get a list of instance vars.
+
+# :attr_accessor :volume
+
+# 7. do puts object. default value is the object name and object_id.
+
+# 8. self refers to the calling object which much be an instance of class Cat.
+
+# 9. It refers to the class itself. this makes cats_count a class method. 
+
+# 10. Bag.new("color", "material")
---
+++ rb120/3_lesson/2.rb
@@ -0,0 +1,21 @@
+#1. one of You will eat a nice lunch, You will take a nap soon, or you will stay at work late.
+
+#2. You will visit Vegas, you  will fly to fiji, or you will romp in Rome. 
+
+#3. class.ancesors
+#Orange < Taste< Object < Kernel < BasicObject
+#HotSauce < Taste < Object < Kernel < Basic Object
+
+#4. attr_accessor :type
+
+#5. local var, instance var, class var.
+
+#6. manufacturer. it has self. in its definition. class.class_method
+
+#7. class variable, common across as instances of Cat. Keeps tally of number of cat instances created. Can be tested by created Cat instances and checking if Cat.cats_count increases accordingly.
+
+#8. < Game
+
+#9. will override the play method from Game class.
+
+#10. > modular, > encapsulated, > abstractions, > polymorphism across types > reduce duplication.
