9fbeaad892bb7838ef1220daa1933188a625dc0e
Sush Krishnamoorthy <sushkrsh@gmail.com>
2022-08-22 20:18:38 +0000

    rb120 debugging exercises

---
+++ rb120/exercises/debugging/8.rb
@@ -0,0 +1,50 @@
+class BankAccountError < StandardError; end
+class LowBalanceError < BankAccountError; end
+
+class BankAccount
+  attr_reader :balance
+
+  def initialize(account_number, client)
+    @account_number = account_number
+    @client = client
+    @balance = 0
+  end
+
+  def deposit(amount)
+    if amount > 0
+      self.balance += amount
+      "$#{amount} deposited. Total balance is $#{balance}."
+    else
+      "Invalid. Enter a positive amount."
+    end
+  end
+
+  def withdraw(amount)
+    begin   
+      self.balance -= amount
+      "$#{amount} withdrawn. Total balance is $#{balance}."
+    rescue LowBalanceError => e
+      e.message
+    end
+  end
+
+  def balance=(new_balance)
+    if new_balance >=0
+      @balance = new_balance 
+    else
+      raise LowBalanceError.new("Invalid. Enter positive amount less than or equal to current balance ($#{balance}).")
+    end
+  end
+end
+
+# Example
+
+account = BankAccount.new('5538898', 'Genevieve')
+
+                          # Expected output:
+p account.balance         # => 0
+p account.deposit(50)     # => $50 deposited. Total balance is $50.
+p account.balance         # => 50
+p account.withdraw(80)    # => Invalid. Enter positive amount less than or equal to current balance ($50).
+                          # Actual output: $80 withdrawn. Total balance is $50.
+p account.balance         # => 50
