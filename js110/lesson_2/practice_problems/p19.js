64008d4ef6c3e151d073679bd9c9dc24f60675bd
sushkrish <sushkrsh@gmail.com>
2024-03-22 00:33:05 -0400

    linter fix for js110 lesson 2 practice problem

--- js110/lesson_2/practice_problems/p19.js
+++ js110/lesson_2/practice_problems/p19.js
@@ -10,4 +10,6 @@ let munstersCopy = {};
 for (let key in munsters) {
   munstersCopy[key] = Object.freeze(Object.assign({}, munsters[key]));
 }
+
+munstersCopy.herman.age += 1;
 console.log(munstersCopy);
